5b3194acc907ce5a8203106a89cec29f8bfd5923
Ana Chernov <anastasia.chernov@gmail.com>
2019-10-21 18:49:34 -0400

    Created readme and removed unused assets

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# unit-4-game
-JQuery Game
+# JQuery Game
+
+For this project I created a "Crystal Collector" game using JQuery.
+
+The goal of the game is to match the computer's randomly generated number without going over.
+
+Each of the four crystals is assigned a random concealed value at the start of the game. The player clicks a crystal to increase his total score. Each crystal click results in a cumulative increase of your score. The player wins by matching the computer's random number.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,8 @@ var wins = 0;
 var losses = 0;
 var targetNumber = 0;
 
+// Here I'm creating a function that I will run each time the window loads and each time a win or loss happens
+
 function randomizingFunction() {
   // This will randomly choose targetNumber (between 19 and 120) and set it to our targetNumber
   var random = Math.floor(Math.random() * 120) + 19;
@@ -11,51 +13,56 @@ function randomizingFunction() {
 
   // Assign random numbers to crystals
   var ranCrystal1 = Math.floor(Math.random() * 12) + 1;
-
   var ranCrystal2 = Math.floor(Math.random() * 12) + 1;
-
   var ranCrystal3 = Math.floor(Math.random() * 12) + 1;
-
   var ranCrystal4 = Math.floor(Math.random() * 12) + 1;
 
   totalScore = 0;
 
-  // This is getting the span of the Goal Number and setting it to the text of the variable
+  // This is updating the HTML text of the score and goal number
   $("#goal-number").text(targetNumber);
   $("#score").text(totalScore);
 
+  // This is assigning the random numbers generated in lines 15-18 to a new attribute (value) of each crystal
   $("#crystal1").attr("value", ranCrystal1);
   $("#crystal2").attr("value", ranCrystal2);
   $("#crystal3").attr("value", ranCrystal3);
   $("#crystal4").attr("value", ranCrystal4);
 }
 
+// I'm calling the randomizing function
 randomizingFunction();
 
+// I'm creating a function that will run when a win or loss occurs
 function winCondition() {
   // Setting up a WIN: If total score matches the goal number, you are alerted of a win
   if (totalScore === targetNumber) {
-    ////
     alert("You win!");
+    // Wins increases by one and displays on the page
     wins += 1;
     $("#wins-text")
       .empty()
       .append(wins);
+    // Numbers get reset and a new game starts
     randomizingFunction();
   } else if (totalScore >= targetNumber) {
-    // Then they are alerted with a loss.
+    // Setting up a LOSS: Then they are alerted with a loss.
     alert("You lose!!");
     losses += 1;
     $("#losses-text")
       .empty()
       .append(losses);
+    // Numbers get reset and a new game starts
     randomizingFunction();
   }
 }
 
 // Creating an on-click event that responds when user clicks a crystal
 $(document).on("click", ".crystal-image", function() {
+  // This is checking the value of the crystal that was clicked
   console.log($(this).attr("value"));
+
+  // And increasing the total score by that value
   totalScore += parseInt($(this).attr("value"));
 
   console.log(totalScore);
